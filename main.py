6c34df4985824e94d99864ab0fc0f9356099e222
Rajanya Saha <itsrajanyasaha27@gmail.com>
2025-12-27 17:41:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import json
-from Market_Pulse.news_fetcher import fetch_news
-from Market_Pulse.sentiment_agent import analyze_sentiment
+from news_fetcher import fetch_news
+from sentiment_agent import analyze_sentiment
 
 app = FastAPI(title="Market Pulse API")
 
