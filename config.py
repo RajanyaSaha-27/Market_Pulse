df3a98ad8963dacf89b63208a5ffcca3ea2f00c2
Rajanya Saha <itsrajanyasaha27@gmail.com>
2026-01-13 01:09:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
-import os
 import google.generativeai as genai
+import os
 
-GEMINI_API_KEY = "AIzaSyDMLBjsOhmazET9Y_z4vxD2FBV86nda2HY"
-
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")  
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config={
+        "temperature": 0.2,
+        "response_mime_type": "application/json"  
+    }
+)
